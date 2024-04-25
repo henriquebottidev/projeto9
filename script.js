ca477f9c232e7e3d010ab8e64a24c59c994e6c70
henriquebottidev <henriquembotti@gmail.com>
2024-04-25 09:22:54 -0300

    adicionado o final do projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+// declarando variaveis 
+
+ let p1 = prompt("Digite seu NOME: ")
+ let p2 = prompt("Digite seu PODER: ")
+ let p3 = prompt("Digite o nome de um Vilão: ")
+ let p4 = prompt("Digite um LUGAR: ")
+let msg = document.getElementById("msg")
+
+msg.innerHTML = `Olá ${p1}, o seu poder é ${p2} e você vai enfrentar o ${p3} no/em ${p4}`
